88be842a068d224be4e20b680a03eeb435e367e0
grentank <mexmat100394@gmail.com>
2024-04-11 22:02:18 +0300

    layout-outlet

--- client/src/components/pages/AccountPage.jsx
+++ client/src/components/pages/AccountPage.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import axiosInstance from '../../service/instance';
 
 export default function AccountPage() {
-  // const data = useLoaderData();
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axiosInstance('/posts?second=2', { headers: { second: 'second' } }).then((res) =>
-      setData(res.data.data),
-    );
-  }, []);
+  const data = useLoaderData();
+  // const [data, setData] = useState([]);
+  // useEffect(() => {
+  //   axiosInstance('/posts?second=2', { headers: { second: 'second' } })
+  //     .then((res) => setData(res.data))
+  //     .catch(console.log);
+  // }, []);
   return (
     <div className="row">
       <div className="col">
